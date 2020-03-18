c7d767f9f297bb5e3f49a373ea9520741eb9d479
Brianna Fisher <Bnfisher4@gmail.com>
2020-03-18 17:39:25 -0500

    Adds feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -13,3 +19,14 @@ class Finch(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
