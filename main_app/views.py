1898a372d4ede190f8b6d3b53f062c77ee741a71
Brianna Fisher <Bnfisher4@gmail.com>
2020-03-18 15:03:00 -0500

    adds finches to database

--- main_app/views.py
+++ main_app/views.py
@@ -2,19 +2,6 @@ from django.shortcuts import render
 
 from .models import Finch
 
-class Finch:
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Carmen', 'House', 'chirpy', 3),
-  Finch('Sasha', 'Black Rosy', 'funny looking', 0),
-  Finch('Hector', 'Java Sparrow', 'cool', 4)
-]
-
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -23,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
